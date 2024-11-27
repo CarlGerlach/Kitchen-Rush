14e6a35012509f3474b6d75125b547e7e0e022d5
Finley Hahn <finley.hahn07@gmail.com>
2024-11-27 12:59:32 +0100

    erste zwichenspeicherung sound

--- Hallo_SFML/main.cpp
+++ Hallo_SFML/main.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp> 
+#include <iostream>
+using namespace std;
 
 int main()
 {
@@ -6,13 +9,31 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    // Sound-Puffer und Sound einmal erstellen
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("Button-Click.wav")) // WAV-Datei statt MP3
+    {
+        std::cout << "Fehler beim Laden der Sound-Datei!" << std::endl;
+        return -1;
+        //Falls leer
+    }
+
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
-        {
+        {   
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Sound nur bei einem spezifischen Ereignis abspielen
+            if (event.type == sf::Event::KeyPressed) // Beispiel: Tastendruck
+            {
+                sound.play();
+            }
         }
 
         window.clear();
