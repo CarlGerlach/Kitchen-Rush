fae9635f51343bd66b0b2f04d781841594a93ec9
Finley Hahn <finley.hahn07@gmail.com>
2024-12-03 18:33:07 +0100

    Hintergrundmusik und zwei Buttons dazu

--- Hallo_SFML/main.cpp
+++ Hallo_SFML/main.cpp
@@ -15,7 +15,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Kitchen Rush");
 
     sf::Font font;
-    if (!font.loadFromFile("arial.ttf")) { // Stelle sicher, dass eine Schriftart geladen wird
+    if (!font.loadFromFile("arial.ttf"))
+    { // Stelle sicher, dass eine Schriftart geladen wird
         return -1;
     }
 
@@ -26,21 +27,74 @@ int main()
     Spielfeld* playField = new Spielfeld();
 
     //Sound
-    mySound* testSound = new mySound();
+    mySound* soundManager = new mySound(); // Unbennant von "testSound" weil cooler oder so
 
+    // Hintergrundmusik laden und abspielen
+    if (soundManager->loadHintergrundMusik("Hintergrund-Musik.ogg"))
+    {
+        soundManager->setMusicLautstaerke(10.0f); // Lautstärke auf 10 % setzen, weil sonst zu laut -.-
+
+        soundManager->playHintergrundMusik();
+    }
+    else
+    {
+        cout << "Fehler beim Laden der Hintergrundmusik!" << endl;
+    }
 
 
 
 
 
     // Erstelle einen Button
-    Button button(300, 200, 200, 50, "Klick mich!", font, sf::Color::Blue, sf::Color::White, testSound);
+    Button button(300, 200, 200, 50, "Klick mich!", font, sf::Color::Blue, sf::Color::White, soundManager);
 
     // Setze die OnClick-Funktion
-    button.setOnClick([](){ cout << "Button geklickt!" << endl; });
+    button.setOnClick([]() 
+        { cout << "Button geklickt!" << endl; });
 
 
-  
+
+    // Erstelle Button Start Musik
+    Button buttonMusikStart(300, 350, 200, 50, "Musik Start", font, sf::Color::Blue, sf::Color::White, soundManager);
+
+    // Starte die Musik und gib diesen Text in Konsole aus
+    buttonMusikStart.setOnClick
+    ([soundManager]
+        {
+            cout << "Musik wird gestartet." << endl;
+            soundManager->getMeinSound().play(); // Spiele den Button-Klick-Sound ab
+
+
+            // nur wenn Musik nicht schon spielt
+            if (soundManager->isMusicPlaying()) // Überprüfen, ob Musik spielt
+            {
+                cout << "Die Musik spielt bereits." << endl;
+            }
+            else
+            {
+                soundManager->playHintergrundMusik(); // Starte die Musik
+            }
+        }
+    );
+
+
+
+    // Erstelle Button Stop Musik
+    Button buttonMusikStopp(300, 275, 200, 50, "Musik Stop", font, sf::Color::Blue, sf::Color::White, soundManager);
+
+    // Stoppe die Musik und gib diesen Text in Konsole aus
+    buttonMusikStopp.setOnClick
+    ([soundManager]
+        {
+            cout << "Musik wird gestoppt." << endl;
+            soundManager->getMeinSound().play(); // Spiele den Button-Klick-Sound ab
+            soundManager->stopHintergrundMusik(); // Stoppe die Musik
+        }
+    );
+
+    
+
+   
 
 
 
@@ -58,12 +112,16 @@ int main()
             // Sound nur bei einem spezifischen Ereignis abspielen
             if (event.type == sf::Event::KeyPressed) // Beispiel: Tastendruck
             {
-                testSound->getMeinSound().play();
+                soundManager->getMeinSound().play();
             }
+
+           
 
 
             // Verarbeite Button-Klick
             button.handleEvent(event, window);
+            buttonMusikStopp.handleEvent(event, window);
+            buttonMusikStart.handleEvent(event, window);
 
         }
 
@@ -73,8 +131,15 @@ int main()
 
         button.draw(window);
 
+        buttonMusikStopp.draw(window);
+
+        buttonMusikStart.draw(window);
+
         window.display();
     }
+
+    // Hintergrundmusik stoppen, wenn das Programm beendet wird
+    soundManager->stopHintergrundMusik();
 
     return 0;
 }
