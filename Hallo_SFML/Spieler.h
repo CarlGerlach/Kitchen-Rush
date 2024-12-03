b2e5e9f0fb73a9596ad20ea7c1b2419f9d3df2c2
Leanderlul <lele41613@gmail.com>
2024-12-03 12:14:38 +0100

    Klasse spieler erstellt, Spieler wird erstellt und kann sich nur innerhalb des Spielfeldes bewegen

---
+++ Hallo_SFML/Spieler.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp> 
+#include <iostream>
+#include <functional>
+class Spieler {
+private:
+    sf::RectangleShape shape; // Form des Spielers 
+    float speed; // Bewegungsgeschwindigkeit
+    sf::FloatRect bounds; //Spielfeldgrenzen
+
+public:
+    Spieler(float startX, float startY, float size, float speed, sf::FloatRect bounds)
+        : speed(speed), bounds(bounds) {
+        shape.setSize(sf::Vector2f(size, size));
+        shape.setFillColor(sf::Color::Blue);
+        shape.setPosition(startX, startY);
+    }
+
+    // Funktion zum Zeichnen des Spielers
+    void draw(sf::RenderWindow& window) {
+        window.draw(shape);
+    }
+
+    // Funktion zur Bewegung
+    void move(sf::Vector2f direction) {
+        if (direction.x != 0.f || direction.y != 0.f) {
+            // Länge des Vektors berechnen (Pythagoras)
+            float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+
+            // Richtung normalisieren
+            direction /= length;
+
+            // Neue Position berechnen
+            sf::Vector2f newPosition = shape.getPosition() + direction * speed;
+
+            // Begrenzungen prüfen
+            if (newPosition.x < bounds.left) newPosition.x = bounds.left;
+            if (newPosition.y < bounds.top) newPosition.y = bounds.top;
+            if (newPosition.x + shape.getSize().x > bounds.left + bounds.width)
+                newPosition.x = bounds.left + bounds.width - shape.getSize().x;
+            if (newPosition.y + shape.getSize().y > bounds.top + bounds.height)
+                newPosition.y = bounds.top + bounds.height - shape.getSize().y;
+
+            shape.setPosition(newPosition);
+        }
+
+        
+    }
+
+    // Zugriff auf die Position
+    sf::Vector2f getPosition() {
+        return shape.getPosition();
+    }
+};
+
--- Hallo_SFML/main.cpp
+++ Hallo_SFML/main.cpp
@@ -6,6 +6,7 @@
 #include "Spielfeld.h"
 #include "Button.h"
 #include "mySound.h"
+#include "Spieler.h"
 
 using namespace std;
 using namespace sf;
@@ -40,7 +41,10 @@ int main()
     button.setOnClick([](){ cout << "Button geklickt!" << endl; });
 
 
-  
+    // Spielfeldbegrenzung (x, y, Breite, Höhe)
+    sf::FloatRect spielfeldGrenzen(200.f, 170.f, 1460.f, 730.f);
+    //erstellt Spieler
+    Spieler spieler1(300.f, 300.f, 50.f, 0.6f, spielfeldGrenzen);
 
 
 
@@ -67,9 +71,32 @@ int main()
 
         }
 
+
+
+        // Spieler bewegen (mit WASD)
+        sf::Vector2f direction(0.f, 0.f);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            direction.y -= 1.f; // Nach oben
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            direction.y += 1.f; // Nach unten
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            direction.x -= 1.f; // Nach links
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            direction.x += 1.f; // Nach rechts
+        }
+
+        spieler1.move(direction);
+
+
+
         window.clear();
  
         playField->drawSpielfeld(window);
+
+        spieler1.draw(window);
 
         button.draw(window);
 
