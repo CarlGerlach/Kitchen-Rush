4e4dddc23d4a84d8ededd8aa606ec1d6753cb2aa
Leanderlul <lele41613@gmail.com>
2025-01-21 21:00:26 +0100

    Klasse Item hinzugefügt

---
+++ Hallo_SFML/Item.cpp
@@ -0,0 +1,18 @@
+#include "Item.h"
+
+Item::Item(const std::string& type, const sf::Texture& texture) : typ(type)
+{
+	sprite.setTexture(texture);
+}
+
+
+
+string Item::getTyp() 
+{
+    return typ;
+}
+
+sf::Sprite Item::getSprite()
+{
+    return sprite;
+}
